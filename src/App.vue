1c214b279f0bde525cf31f07588cec9b095ab295
umudik <seyitumutbicer@gmail.com>
2022-12-08 01:26:52 +0300

    dev

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <script setup>
 import { useStore } from "./store";
+import { onMounted, ref } from "vue";
 import lodash from "lodash";
 const store = useStore();
+const models = ref([]);
+onMounted(async () => {
+  const res = await store.run({
+    model: "model",
+    method: "read",
+  });
+  models.value = res.data;
+});
 </script>
 
 <template>
   <v-app>
     <v-main>
       <v-text-field v-model="store.$state.url" />
-      <div v-for="m of store.list('model')">{{ m }}</div>
+      <v-text-field v-model="store.$state.token" />
+      <div v-for="m of models">{{ m.name }}</div>
     </v-main>
   </v-app>
 </template>
--- src/store/index.js
+++ src/store/index.js
@@ -4,20 +4,20 @@ export const useStore = defineStore("counter", {
     state: () => {
         return {
             entites: {
-                model: ["a", "b", "c"],
+                model: [],
                 database: [],
                 lifecycle: [],
                 setting: [],
                 mixin: [],
             },
             url: "http://localhost:2626",
-            token: "admin",
+            token: "umudik",
         };
     },
     actions: {
         list(model) { return this.$state.entites[model] },
         async run(payload) {
-            const { url } = this.$state
+            const { url, token } = this.$state
             const res = await axios.post(url, {
                 ...payload, token
             })
