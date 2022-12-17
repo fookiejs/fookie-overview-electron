feb886b203f94b14ef2976f9d32515539bd971a1
umudik <seyitumutbicer@gmail.com>
2022-12-17 22:39:05 +0300

    dev

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,10 +1,8 @@
 // Plugins
 import { loadFonts } from './webfontloader'
 import vuetify from './vuetify'
-import pinia from './pinia'
 
 export function registerPlugins(app) {
   loadFonts()
   app.use(vuetify)
-  app.use(pinia)
 }
